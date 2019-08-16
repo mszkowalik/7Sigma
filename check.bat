21e8a3bf23bc4a4d15bdb78fea8503cb63d86a0d
Mateusz Kowalik <msz.kowalik@gmail.com>
2019-08-16 13:10:37 +0200

    As usually shitload of changes

--- check.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0\_tools\python\scripts\env_for_icons.bat"
-
-
-call "%~dp0\_tools\run_python.bat" ".\_tools\export_csv.py"
---
+++ export.bat
@@ -0,0 +1,5 @@
+@echo off
+call "%~dp0\_tools\python\scripts\env_for_icons.bat"
+
+
+call "%~dp0\_tools\run_python.bat" ".\_tools\export_csv.py"
