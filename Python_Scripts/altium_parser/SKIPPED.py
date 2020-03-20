ec31b69847308f4aeeda17b21718ac472c31c2f4
Mateusz Kowalik <msz.kowalik@gmail.com>
2020-03-20 23:33:55 +0100

    Dodałem metalowe inserty WA-SMSR 2.6mm i 3.6mm, poprawiłem Testpoint 1.3mm i 1.5mm (zmiena nazwy)

--- Python_Scripts/altium_parser/SKIPPED.py
+++ Python_Scripts/altium_parser/SKIPPED.py
@@ -4,7 +4,9 @@ test_pin_length_should_be_200_mils_SKIPPED = \
 test_datasheet_valid_url_SKIPPED = {
     'T-CONNECT FEMALE',
     'T-CONNECT MALE',
-    'TP_PAD_2MM'
+    'TP_PAD_2MM',
+    'TP_PAD_1.5MM',
+    'TP_PAD_1.3MM'
     }
 
 test_pin_designators_shoud_be_visible_SKIPPED = {}
